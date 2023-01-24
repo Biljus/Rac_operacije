5aa899c39b1ea9ea0a375ab57ddf82af0e7b7396
Biljus <b.uci.codiranje@gmail.com>
2023-01-24 15:56:12 +0100

    first commit

---
+++ rac_op.js
@@ -0,0 +1,26 @@
+function addition() {
+    a=Number(document.mojracun.prvi.value) ;
+    b=Number(document.mojracun.drugi.value) ;
+    c=a+b;
+    document.mojracun.total.value=c;
+}
+
+function substraction() {
+    a=Number(document.mojracun.prvi.value) ;
+    b=Number(document.mojracun.drugi.value) ;
+    c=a-b;
+    document.mojracun.total.value=c;
+}
+function multiply() {
+    a=Number(document.mojracun.prvi.value) ;
+    b=Number(document.mojracun.drugi.value) ;
+    c=a*b;
+    document.mojracun.total.value=c;
+}
+
+function division() {
+    a=Number(document.mojracun.prvi.value) ;
+    b=Number(document.mojracun.drugi.value) ;
+    c=a/b;
+    document.mojracun.total.value=c;
+}
